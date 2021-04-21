3cfc060344641ca304e12dd2731a9dbd0687a711
matheus-nicolay <nicolaymatheus222@gmail.com>
2021-04-21 16:06:25 -0300

    added all files

---
+++ articles/Article.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = require("../categories/Category")
+
+const Article = connection.define('articles', {
+    title:{
+       type: Sequelize.STRING,
+       allowNull: false 
+    }, slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Category.hasMany(Article); //1-n hasMany
+Article.belongsTo(Category); //1-1 belognsTo
+
+//Article.sync({force: true});
+
+module.exports = Article;
---
+++ articles/ArticlesController.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const Category = require("../categories/Category");
+const router = express.Router();
+const Article = require("./Article");
+const slugify = require("slugify");
+
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/index",{articles: articles})
+    });
+});
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories})
+    });  
+});
+
+router.get("/admin/articles/edit/:slug", (req, res) => {
+    var slug = req.params.slug;
+    Category.findAll().then(categories => {
+        Article.findOne({ where: { slug: slug } }).then(article =>{
+                if(article != undefined){
+                    res.render("admin/articles/edit", {article: article}, {categories: categories});
+                }else{
+                    res.redirect("/admin/articles");
+                } 
+        }).catch(erro => {
+            res.redirect("/admin/articles");
+        });
+    }); 
+});
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var id = req.body.category;
+
+    if(title != undefined || body != undefined || id != undefined){
+        Article.create({
+            title: title,
+            slug: slugify(title),
+            body: body,
+            categoryId: id
+        }).then(() =>{
+            res.redirect("/admin/articles");
+        });
+
+    }else{
+        res.redirect("/admin/articles/new");
+    }
+});
+
+
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() =>{
+                res.redirect("/admin/articles");
+            });
+
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }else{
+        res.redirect("/admin/articles");
+    }
+});
+
+module.exports = router;
